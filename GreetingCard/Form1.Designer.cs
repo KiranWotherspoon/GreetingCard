e189065e7c35a1dcc51dbb21c4e416559e1faf47
Kiran Wotherspoon <kirawoth947@ed.amdsb.ca>
2018-02-14 10:02:40 -0800

    Add project files.

---
+++ GreetingCard/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace GreetingCard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.titleLabel.Location = new System.Drawing.Point(85, 126);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(420, 54);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Leo Greeting Card";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(12, 550);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(219, 27);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Kiran Wotherspoon";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(591, 586);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.titleLabel);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Leo";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label nameLabel;
+    }
+}
+
---
+++ GreetingCard/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreetingCard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            //make labels disappear
+            titleLabel.Visible = false;
+            nameLabel.Visible = false;
+
+            //set up for graphics
+            Graphics insideCard = this.CreateGraphics();
+            insideCard.Clear(Color.Black);
+            Pen starPen = new Pen(Color.Yellow, 3);
+            SolidBrush starBrush = new SolidBrush(Color.White);
+            SolidBrush wordBrush = new SolidBrush(Color.Blue);
+
+            //draw strings
+            Font titleFont = new Font("Verdana", 20, FontStyle.Bold);
+            Font bodyFont = new Font("Cambria", 12, FontStyle.Regular);
+            insideCard.DrawString("The Leo Constellation", titleFont, wordBrush, 10, 10);
+
+            //draw constellation
+            insideCard.DrawLine(starPen, 455, 25, 505, 75); //draw lines
+            insideCard.DrawLine(starPen, 455, 25, 345, 145);
+            insideCard.DrawLine(starPen, 345, 145, 365, 205);
+            insideCard.DrawLine(starPen, 363, 205, 143, 265);
+            insideCard.DrawLine(starPen, )
+            insideCard.FillEllipse(starBrush, 450, 20, 10, 10); //draw stars
+            insideCard.FillEllipse(starBrush, 500, 70, 10, 10);
+            insideCard.FillEllipse(starBrush, 340, 140, 10, 10);
+            insideCard.FillEllipse(starBrush, 360, 200, 10, 10);
+            insideCard.FillEllipse(starBrush, 140, 260, 10, 10);
+            insideCard.FillEllipse(starBrush, 40, 370, 10, 10);
+            insideCard.FillEllipse(starBrush, 180, 340, 10, 10);
+            insideCard.FillEllipse(starBrush, 460, 230, 10, 10);
+            insideCard.FillEllipse(starBrush, 510, 290, 10, 10);
+        }
+    }
+}
